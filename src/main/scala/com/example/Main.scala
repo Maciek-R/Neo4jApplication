503fd1ab0120696b3d6a818838dddf5229ef0c7a
Maciej Ruszczyk <ruszczyk.maciek@gmail.com>
2023-03-12 21:22:01 +0100

    Added Http server

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,18 +1,47 @@
 package com.example
 
+import cats.effect.IO
+import org.http4s.HttpRoutes
 import org.neo4j.driver.GraphDatabase
+import cats.effect._
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.ember.server.EmberServerBuilder
+import com.comcast.ip4s._
+import org.http4s.server.Router
 
-object Main {
-  def main(args: Array[String]) = {
-    val appConfig = AppConfig(DataBaseConfig("bolt://localhost/7687", Neo4jCredentials("neo4j", "neo4jneo4j")))
-    val userRepository: UserRepository = new UserRepositoryImpl(appConfig)
+object Main extends IOApp {
 
-    val created = userRepository.create(User.User("mac", None))
-    val usersByName = userRepository.getByName("mac")
+  val appConfig = AppConfig(
+    DataBaseConfig(
+      "bolt://localhost/7687",
+      Neo4jCredentials("neo4j", "neo4jneo4j")
+    )
+  )
+  val userRepository: UserRepository = new UserRepositoryImpl(appConfig)
 
-    val users = userRepository.readAll()
-    users.foreach(println)
-    println("---------------------")
-    usersByName.foreach(println)
+  val service = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name => Ok(s"Hello $name")
+    case GET -> Root / "users" =>
+      val users = userRepository.readAll()
+      Ok(users.mkString("\n")) // TODO add encoders
+    case GET -> Root / "users" / "name" / name =>
+      val users = userRepository.getByName(name)
+      Ok(users.mkString("\n"))
+    case GET -> Root / "users" / "create" / name / lastName => // TODO change it to POST
+      val created = userRepository.create(User.User(name, Some(lastName)))
+      Ok(created.toString)
+  }
+  val httpApp = Router("/" -> service).orNotFound
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(httpApp)
+      .build
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
   }
 }
